01a3a14b85bcc49e3c849feefa2105bf5dd45832
SHANKAR-PROJECT <kingshankar1234@yahoo.com>
2024-06-04 03:35:08 +0530

    Update out.js

--- script/commands/out.js
+++ script/commands/out.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "out",
     version: "1.0.0",
-    hasPermssion: 2,
+    hasPermission: 2,
     credits: "SHANKAR SUMAN",
     description: "Leave the group",
     usePrefix: false,
