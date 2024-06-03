04b6a3349fbde6fddd874308da561a1436625a25
SHANKAR-PROJECT <kingshankar1234@yahoo.com>
2024-06-04 03:42:54 +0530

    Update out.js

--- script/commands/out.js
+++ script/commands/out.js
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.run = async function({ api, event, args }) {
     const leaveCommands = ["chal nikal", "chal bhag", "nikal", "out", "chal bhag yaha se", "taklu bhag jaa"];
-    const command = args.join(" ").toLowerCase();
+    const command = event.body.toLowerCase().replace(/\s+/g, ' ').trim();
     if (leaveCommands.includes(command)) {
         await api.sendMessage("जो आज्ञा मेरे मालिक😍", event.threadID);
         return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
