76bd8377dfbfa2d4ffc4f8e5ef35593d821d7a56
shankar852139 <kingshankar1234@yahoo.com>
2024-06-03 07:29:24 +0530

    Update 0-IMGBB.js

--- script/commands/0-IMGBB.js
+++ script/commands/0-IMGBB.js
@@ -1,23 +1,23 @@
 module.exports.config = {
-    name: "imgbb",
+    name: "imgur",
     version: "1.0.0",
-    hasPermssion: 0,
-    credits: "SHANKAR SUMAN",
-    description: "imgbb uploader(limited)",
-    commandCategory: "tools",
+    hasPermssion: 2,
+    credits: "ZiaRein",
+    description: "imgur upload",
+    commandCategory: "link",
     usePrefix: false,
-    usages: "[reply to image]",
-    cooldowns: 5,
+    usages: `Please reply to image\n\nHow to use?\n${global.config.PREFIX}imgur [reply] <img>\n\nExample:\n${global.config.PRFIX}imgur <img reply>\n`,
+    cooldowns: 1,
     dependencies: {
   "axios": "",}
 };
  
 module.exports.run = async ({ api, event }) => {
 const axios = global.nodemodule['axios'];  
-var linkanh = event.messageReply.attachments[0].url || args.join(" ");
-    if(!linkanh) return api.sendMessage('Please reply to image.', event.threadID, event.messageID)
-const res = await axios.get(`http://zekais-api.herokuapp.com/imgbb?url=${encodeURIComponent(linkanh)}&apikey=N3MqSq2j`);    
-var juswa = res.data.uploaded.image;
-    return api.sendMessage(`Here is your imgur link:\n\n${juswa}`, event.threadID, event.messageID);
+var ZiaRein = event.messageReply.attachments[0].url || args.join(" ");
+    if(!ZiaRein) return api.sendMessage(`Please reply to image\n\nHow to use?\n${global.config.PREFIX}imgur [reply] <img>\n\nExample:\n${global.config.PRFIX}imgur <img reply>\n\nCreated by: Shankar suman`, event.threadID, event.messageID)
+const res = await axios.get(`https://api.phamvandien.xyz/imgur?link=${encodeURIComponent(ZiaRein)}`);    
+var ZiaReinn = res.data.uploaded.image;
+    return api.sendMessage(ZiaReinn, event.threadID, event.messageID);
  
 }
