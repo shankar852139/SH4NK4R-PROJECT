2c69884a611941647a7097465fb4e4fa250bbdb7
SHANKAR-PROJECT <kingshankar1234@yahoo.com>
2024-06-18 16:44:32 +0530

    Update 0-STALK2.js

--- script/commands/0-STALK2.js
+++ script/commands/0-STALK2.js
@@ -1,64 +1,86 @@
-var cred = "SHANKAR";
-
+/*Wag nyo palitan credits mga bwakanangshit.*/
 module.exports.config = {
-    name: "stalk2",
-    version: "1.0.0",
-    hasPermision: 0,
-    credits: `${cred}`,
-    description: "get info using uid/mention/reply to a message",
-    usages: "[reply/uid/@mention/url]",
-    commandCategory: "info",
-    cooldowns: 0,
+  name: "stalk2",
+  version: "1.0.0",
+  hasPermssion: 0, //permission gawing 1 kung gusto nyo admin ng gc at admin ng bot lang, gawin namang 2 if gusto nyo admin bot lang 
+  credits: "Joshua Sy", /*Wag nyo palitan credits mga bwakanangshit(2).*/
+  description: "View information of facebook users",
+  commandCategory: "info",
+  usePrefix: false,  
+  usages: "or reply to a message or @mention",
+  cooldowns: 0 //cooldown
 };
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+     if(!args[0]){
+    if(event.type == "message_reply") { uid = event.messageReply.senderID }
+    else uid = event.senderID;
+   const res = await axios.get(`https://manhict.tech/api/fbInfo?id=${uid}&apikey=lgG765KO`); //may limit 500 lang then pag nagamit na ung 500 antay ulit 12 hrs :3
+    var id = res.data.result.id; 
+    var name = res.data.result.name; 
+    var fname = res.data.result.firstName;
+    var username = res.data.result.vanity; 
+    var bday = res.data.result.birthday;
+    var f = res.data.result.follow;
+    var url = res.data.result.profileUrl;
+    var ht = res.data.result.hometown;
+    var loc = res.data.result.location;
+    var rs = res.data.result.love;
+    var quotes = res.data.result.quotes;
+    var data = res.data.result.thumbSrc;
+    var callback = () => api.sendMessage({body:`Name: ${name}\nFirst Name: ${fname}\nFacebook Username: ${fname}\nBirthday: ${bday}\nFollowers: ${f}\nLocation: ${loc}\nRelationship: ${rs}\nProfile Url: ${url}\nHometown: ${ht}\nUID: ${id}`,
+        attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+        () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+    return request(encodeURI(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=463372798834978|csqGyA8VWtIhabZZt-yhEBStl9Y`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+        () => callback());
+   }
 
-module.exports.run = async function({api, event, args, utils, Users, Threads}) {
-    try {
-        // Check if credits have been changed
-        if (module.exports.config.credits !== cred) {
-            return api.sendMessage(`Credits have been changed from ${cred}.`, event.threadID, event.messageID);
-        }
-
-        let axios = require('axios');
-        let fs = require("fs-extra");
-        let request = require("request");
-        let {threadID, senderID, messageID} = event;
-
-        let id;
-        if (args.join().indexOf('@') !== -1) {
-            id = Object.keys(event.mentions)[0];
-        } else {
-            id = args[0] || event.senderID;
-        }
-
-        if (event.type == "message_reply") {
-            id = event.messageReply.senderID;
-        } else if (args.join().indexOf(".com/") !== -1) {
-            const res = await axios.get(`https://api.reikomods.repl.co/sus/fuid?link=${args.join(" ")}`);
-            id = res.data.result;
-        }
-
-        let userInfo = await api.getUserInfo(id);
-        let name = userInfo[id].name;
-        let username = userInfo[id].vanity === undefined ? "Not Found" : userInfo[id].vanity;
-        let url = userInfo[id].profileUrl;
-
-        // Fetch additional information
-        const userDetails = await axios.get(`https://graph.facebook.com/${id}?fields=birthday,gender,location,hometown&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
-        let gender = userDetails.data.gender ? userDetails.data.gender.charAt(0).toUpperCase() + userDetails.data.gender.slice(1) : "Not Found";
-        let birthday = userDetails.data.birthday ? userDetails.data.birthday : "Not Found";
-        let location = userDetails.data.location ? userDetails.data.location.name : "Not Found";
-        let hometown = userDetails.data.hometown ? userDetails.data.hometown.name : "Not Found";
-
-        let callback = function() {
-            return api.sendMessage({
-                body: `•——[INFORMATION]——•\n\nName: ${name}\nFacebook URL: https://facebook.com/${username}\nUID: ${id}\nGender: ${gender}\nBirthday: ${birthday}\nLocation: ${location}\nHometown: ${hometown}\n\n•——[INFORMATION]——•`,
-                attachment: fs.createReadStream(__dirname + `/cache/image.png`)
-            }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/image.png`), event.messageID);
-        };
-
-        return request(encodeURI(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(fs.createWriteStream(__dirname + `/cache/image.png`)).on("close", callback);
-    } catch (err) {
-        console.log(err);
-        return api.sendMessage(`Error`, event.threadID);
+    else {
+    if (args.join().indexOf('@') !== -1){
+    var mentions = Object.keys(event.mentions)
+    const res = await axios.get(`https://manhict.tech/api/fbInfo?id=${mentions}&apikey=lgG765KO`);
+    var id = res.data.result.id;
+    var name = res.data.result.name;
+    var fname = res.data.result.firstName;
+    var username = res.data.result.vanity;
+    var bday = res.data.result.birthday;
+    var f = res.data.result.follow;
+    var url = res.data.result.profileUrl;
+    var ht = res.data.result.hometown;
+    var loc = res.data.result.location;
+    var rs = res.data.result.love;
+    var quotes = res.data.result.quotes;
+    var data = res.data.result.thumbSrc;
+    var callback = () => api.sendMessage({body:`Name: ${name}\nFirst Name: ${fname}\nFacebook Username: ${fname}\nBirthday: ${bday}\nFollowers: ${f}\nLocation: ${loc}\nRelationship: ${rs}\nProfile Url: ${url}\nHometown: ${ht}\nUID: ${id}`,
+        attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+        () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+    return request(encodeURI(`https://graph.facebook.com/${mentions}/picture?height=1500&width=1500&access_token=463372798834978|csqGyA8VWtIhabZZt-yhEBStl9Y`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+        () => callback());
     }
-}
+    else {
+    const res = await axios.get(`https://manhict.tech/api/fbInfo?id=${args[0]}&apikey=lgG765KO`);
+    var id = res.data.result.id;
+    var name = res.data.result.name;
+    var fname = res.data.result.firstName;
+    var username = res.data.result.vanity;
+    var bday = res.data.result.birthday;
+    var f = res.data.result.follow;
+    var url = res.data.result.profileUrl;
+    var ht = res.data.result.hometown;
+    var loc = res.data.result.location;
+    var rs = res.data.result.love;
+    var quotes = res.data.result.quotes;
+    var data = res.data.result.thumbSrc;
+    var callback = () => api.sendMessage({body:`Name: ${name}\nFirst Name: ${fname}\nFacebook Username: ${fname}\nBirthday: ${bday}\nFollowers: ${f}\nLocation: ${loc}\nRelationship: ${rs}\nProfile Url: ${url}\nHometown: ${ht}\nUID: ${id}`,
+        attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID,
+        () => fs.unlinkSync(__dirname + "/cache/1.png"),event.messageID); 
+    return request(encodeURI(`https://graph.facebook.com/${args[0]}/picture?height=1500&width=1500&access_token=463372798834978|csqGyA8VWtIhabZZt-yhEBStl9Y`)).pipe(fs.createWriteStream(__dirname+'/cache/1.png')).on('close',
+        () => callback());
+    }
+  }
+  }
+
+
+/*remade and fix by Joshua Sy pogi UWUW*/
