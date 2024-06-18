41e80652853e07456b585f2f1fbac3b28fd4e56c
SHANKAR-PROJECT <kingshankar1234@yahoo.com>
2024-06-19 04:23:18 +0530

    Update translate.js

--- script/commands/translate.js
+++ script/commands/translate.js
@@ -1,50 +1,62 @@
 module.exports.config = {
-    name: "trans",
-    version: "1.0.0",
-    hasPermssion: 0,
-    credits: "SHANKAR",
-    description: "Translate text to another language",
-    commandCategory: "translator",
-    usages: "[Text]",
-    cooldowns: 5,
-    dependencies: {
-        "request": ""
-    }
+  name: "trans",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "Mirai Team (Modified by Yan Maglinte)",
+  description: "Text translation",
+  usePrefix: false,
+  commandCategory: "media",
+  usages: `${global.config.PREFIX}trans fr Hello, how are you?`,
+  cooldowns: 5,
+  dependencies: {
+    "request": ""
+  }
 };
 
-module.exports.run = async ({ api, event, args }) => {
-    const request = global.nodemodule["request"];
-    var content = args.join(" ");
-    if (content.length == 0 && event.type != "message_reply") return global.utils.throwError(this.config.name, event.threadID, event.messageID);
-    
-    var translateThis;
-    var lang;
-    
-    if (event.type == "message_reply") {
-        translateThis = event.messageReply.body;
-        if (content.indexOf("-> ") !== -1) {
-            lang = content.substring(content.indexOf("-> ") + 3);
-        } else {
-            lang = 'hi'; // Default language set to Hindi
-        }
-    } else {
-        if (content.indexOf(" -> ") == -1) {
-            translateThis = content;
-            lang = 'hi'; // Default language set to Hindi
-        } else {
-            translateThis = content.slice(0, content.indexOf(" ->"));
-            lang = content.substring(content.indexOf(" -> ") + 4);
-        }
-    }
+/* Modified Code!
+Here's how to use it:
+"trans [Target Language] [Text]"
 
-    return request(encodeURI(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&q=${translateThis}`), (err, response, body) => {
-        if (err) return api.sendMessage("Error in translation", event.threadID, event.messageID);
-        var retrieve = JSON.parse(body);
-        var text = '';
-        retrieve[0].forEach(item => {
-            if (item[0]) text += item[0];
-        });
-        var fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0];
-        api.sendMessage(`Translation: ${text}\n - Translated from ${fromLang} to ${lang}`, event.threadID, event.messageID);
-    });
+Try this example and check out the results:
+
+Translate "Hello, how are you?" into French:
+• "<prefix>trans fr Hello, how are you?"
+
+Translate "I love you" into Spanish:
+• "<prefix>trans es I love you"
+
+Translate "Good morning" into German:
+• "<prefix>trans de Good morning"
+
+Modified by Yan Maglinte */
+
+module.exports.run = async ({ api, event, args }) => {
+  const request = global.nodemodule["request"];
+  const targetLanguage = args[0];
+  const content = args.slice(1).join(" ");
+
+  if (content.length === 0 && event.type !== "message_reply")
+    return global.utils.throwError(this.config.name, event.threadID, event.messageID);
+
+  let translateThis, lang;
+
+  if (event.type === "message_reply") {
+    translateThis = event.messageReply.body;
+    lang = targetLanguage || global.config.language;
+  } else {
+    translateThis = content;
+    lang = targetLanguage || global.config.language;
+  }
+
+  return request(encodeURI(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&q=${translateThis}`), (err, response, body) => {
+    if (err)
+      return api.sendMessage("An error has occurred!", event.threadID, event.messageID);
+
+    const retrieve = JSON.parse(body);
+    let text = '';
+    retrieve[0].forEach(item => (item[0]) ? text += item[0] : '');
+    const fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0];
+
+    api.sendMessage(`❯ Translation: ${text}\n - Translated from ${fromLang} to ${lang}`, event.threadID, event.messageID);
+  });
 };
