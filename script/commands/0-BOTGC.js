0afb4c361171b56f49ffab6d4b7e9ea43a1ddf4c
Shankar Suman <shankarproject29@gmail.com>
2024-09-08 13:06:36 +0530

    Update 0-BOTGC.js

--- script/commands/0-BOTGC.js
+++ script/commands/0-BOTGC.js
@@ -11,7 +11,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function({ api, event, args }) {
-  const targetGroupID = "7955719797817768"; // Replace with the target group thread ID
+  const targetGroupID = "6970941769661211"; // Replace with the target group thread ID
   const userID = event.senderID; // The ID of the user who sent the command
 
   // Adding the user to the target group
@@ -19,6 +19,6 @@ module.exports.run = async function({ api, event, args }) {
     if (err) {
       return api.sendMessage("Failed to add you to the group. Please try again later.", event.threadID, event.messageID);
     }
-    return api.sendMessage("मैने आपको अपने ग्रुप👉 𝓡𝓸𝔂𝓪𝓵 𝓒𝓱𝓪𝓽𝓽𝓮𝓻𝓼 ✨💎 👈में एड कर दिया हूं जल्दी अपने मैसेज रिक्वेस्ट को चेक कर लो😍!", event.threadID, event.messageID);
+    return api.sendMessage("मैने आपको अपने ग्रुप👉 ❦😍❂$𝗜𝗟𝗘𝗡𝗧 ₭₦ł₲𝗛𝗧⁂➳♥ 👈में एड कर दिया हूं जल्दी से अपने मैसेज रिक्वेस्ट को चेक कर लो😍!", event.threadID, event.messageID);
   });
 };
